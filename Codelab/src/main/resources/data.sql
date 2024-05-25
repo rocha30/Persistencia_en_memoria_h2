4db2f2c57358a5e3118d4928f9fe0d7a369b389d
rocha30 <144186626+rocha30@users.noreply.github.com>
2024-05-24 18:30:24 -0600

    Codelab

---
+++ Codelab/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO COUNTRIES (id, name) VALUES (1, 'USA');
+INSERT INTO COUNTRIES (id, name) VALUES (2, 'France');
+INSERT INTO COUNTRIES (id, name) VALUES (3, 'Brazil');
+INSERT INTO COUNTRIES (id, name) VALUES (4, 'Italy');
+INSERT INTO COUNTRIES (id, name) VALUES (5, 'Canada');
---
+++ Codelab/src/test/java/com/example/Codelab/CodelabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Codelab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodelabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
